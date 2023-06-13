7e5dcdde299160ae23c414d781408e960ea89341
Thomas Sontag <thomas_sontag@ens.univ-artois.fr>
2023-06-13 15:40:38 +0200

    ajout du footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { AProposComponent } from './a-propos/a-propos.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    AProposComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
