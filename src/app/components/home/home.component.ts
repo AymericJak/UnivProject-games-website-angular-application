00c4af8f099c8070487bdc024087f2304ee181ad
Sébastien Coze <sebastien.coze.62@outlook.fr>
2023-06-14 16:16:31 +0200

    Fix home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,21 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {UsersService} from "../../services/users/users.service";
-import {UserRequest} from "../../models/UserRequest";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor(private userService: UsersService) {
-  }
-
-  ngOnInit(): void {
-    this.userService.getUser().subscribe((userRequest: UserRequest) => {
-      console.log(userRequest.adherent['id']);
-    });
-  }
+  constructor() { }
 
 }
