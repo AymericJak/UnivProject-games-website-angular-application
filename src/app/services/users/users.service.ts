c38e048ce4aea9a1c30ea149d92b6bd4a04e8692
Sébastien Coze <sebastien.coze.62@outlook.fr>
2023-06-14 16:22:07 +0200

    Add users service

--- src/app/services/users/users.service.ts
+++ src/app/services/users/users.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {catchError, Observable} from "rxjs";
 import {TokenStorageService} from "../../token-storage.service";
 import {UserRequest} from "../../models/UserRequest";
+import {environment} from "../../environment";
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,7 @@ export class UsersService {
    *
    * @private
    */
-  private static readonly API_URL: string = 'http://localhost:8000/api/';
+  private static readonly API_URL: string = environment.apiUrl + '/';
 
   /**
    * API URLs used for get users list.
@@ -28,71 +29,6 @@ export class UsersService {
   };
 
   constructor(private http: HttpClient, private tokenStorageService: TokenStorageService) { }
-
-  public getUserObservable(id: number = 0): Observable<any> {
-    const token: string = this.tokenStorageService.getToken();
-    const httpOptions: {} = {
-      headers: new HttpHeaders(
-        {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-          'Authorization': 'bearer' + token,
-        }
-      ),
-    };
-    if (id === 0)
-      return this.http.get<any>(`${UsersService.API_URL_DICT['profile']}`, httpOptions);
-    else
-      return this.http.get<any>(`${UsersService.API_URL_DICT['profile']}/${id}`, httpOptions);
-  }
-
-  /*
-  public getUser(id: number = 0): any {
-    let userRequest: UserRequest;
-    this.getUserObservable(id).subscribe(
-      (response): void => {
-        if (response.status === 'success') {
-          const adherent = response['adherent'];
-          console.log(response['status']);
-          userRequest = {
-            status: response.status,
-            message: response.message,
-            adherent: response.adherent,
-            commentaires: response.commentaires,
-            achats: response.achats,
-            likes: response.likes,
-          };
-          console.log(userRequest);
-        } else {
-          alert('ERROR : Indisponibilité de nos services');
-        }
-      },
-      (error): void => {
-        console.error(error);
-      }
-    );
-    return this.getUserObservable(id).pipe(
-      map((response) => {
-        if (response.status === 'success') {
-          const adherent = response['adherent'];
-          const user: UserRequest = {
-            id: adherent.id,
-            login: adherent.login,
-            email: adherent.email,
-            valid: adherent.valide,
-            firstname: adherent.nom,
-            lastname: adherent.prenom,
-            pseudo: adherent.pseudo,
-            avatar: adherent.avatar,
-          }
-          return user;
-        } else {
-          alert('ERROR : Indisponibilité de nos services');
-          return of([]);
-        }
-      })
-    );
-  }*/
 
   public getUser(id: number = 0): Observable<UserRequest> {
     const token: string = this.tokenStorageService.getToken();
