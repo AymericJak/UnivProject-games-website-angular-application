8aad1bff1937361a5978bd33d4a9c5bfdd8afc66
AymericJak <aymericjakpro@gmail.com>
2023-06-14 10:32:01 +0200

    Add game list compoenent

---
+++ src/app/jeux-liste/jeux-liste.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jeux-liste',
+  templateUrl: './jeux-liste.component.html',
+  styleUrls: ['./jeux-liste.component.css']
+})
+export class JeuxListeComponent {
+
+}
