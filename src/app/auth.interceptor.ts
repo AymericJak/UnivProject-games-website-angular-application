ce1ce24e12684359882d5d764d67a1cf86af9e0c
Maxence Dewadder <maxence_dewadder@ens.univ-artois.fr>
2023-06-14 10:51:45 +0200

    t

---
+++ src/app/auth.interceptor.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+   constructor(private authService: AuthService) {
+     }
+
+   intercept(request: HttpRequest<unknown>, next: HttpHandler):
+    Observable<HttpEvent<unknown>> {
+
+     const user = this.authService.userValue;
+     const isLoggedIn = !!user && user.jwtToken;
+     const isApiUrl = request.url.startsWith(environment.apiUrl);
+     if (isLoggedIn && isApiUrl) {
+       request = request.clone({
+         setHeaders: {Authorization: `Bearer ${user.jwtToken}`}
+       });
+       }
+
+     return next.handle(request);
+     }
+   }
+}
