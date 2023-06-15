10a879c9e765ec1e0c05bb8c2b4e1e72bb652f82
Thomas Sontag <thomas_sontag@ens.univ-artois.fr>
2023-06-15 15:15:19 +0200

    ajout commentaire fonctionne

--- src/app/comment-modal/comment-modal.component.ts
+++ src/app/comment-modal/comment-modal.component.ts
@@ -5,6 +5,9 @@ import { ActivatedRoute } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Jeu } from "../models/jeu";
+import {UsersService} from "../services/users/users.service";
+import {Observable} from "rxjs";
+import {UserRequest} from "../models/UserRequest";
 
 @Component({
   selector: 'app-comment-modal',
@@ -12,33 +15,51 @@ import { Jeu } from "../models/jeu";
   styleUrls: ['./comment-modal.component.css']
 })
 export class CommentModalComponent {
-  commentaire: string = ''; // Ajouter un initialisateur ou attribuer une valeur par défaut
+  commentaire: string = '';
   note: number = 0;
   commentaireForm!: FormGroup;
   jeu: Jeu | undefined;
   etat:string='' ;
-
+  profilCourant: Observable<UserRequest>;
+  user_id: number=0;
   constructor(public dialogRef: MatDialogRef<CommentModalComponent>, public gameService: GameService, private route: ActivatedRoute, private http: HttpClient, private formBuilder: FormBuilder,
-              @Inject(MAT_DIALOG_DATA) public data: { jeu: Jeu }
+              @Inject(MAT_DIALOG_DATA) public data: { jeu: Jeu }, public userService: UsersService
   ) {
     this.jeu = data.jeu;
     this.initCommentaireForm();
     this.fetchData();
+    this.profilCourant = this.userService.getUser();
 
   }
-
+  ngOnInit(): void {
+    const id_jeu = this.jeu?.id;
+    const userObservable: Observable<UserRequest> = this.userService.getUser();
+    if (id_jeu){
+      this.profilCourant = this.userService.getUser(parseInt(String(id_jeu)));
+    }
+    else {
+      this.profilCourant = this.userService.getUser();
+    }
+    userObservable.subscribe((user) => {
+      this.user_id = user.adherent.id;
+    });
+  }
   initCommentaireForm(): void {
     this.commentaireForm = this.formBuilder.group({
       commentaire: ['', Validators.required],
-      note: ['', Validators.required]
+      note: ['', Validators.required],
+      date_com: [''],
+      jeu_id: [''],
+      user_id: [''],
+      etat: ['']
     });
   }
 
   fetchData(): void {
-    this.fetchCommentaire();
+    this.fetchJeu();
   }
 
-  fetchCommentaire(): void {
+  fetchJeu(): void {
     console.log(this.jeu?.id);
     if(this.jeu?.id) {
       this.gameService.getJeu(this.jeu?.id).subscribe({
@@ -54,14 +75,15 @@ export class CommentModalComponent {
 
   saveComment(): void {
     const id_jeu = this.jeu?.id;
-    console.log(id_jeu);
     const url: string = `http://localhost:8000/api/jeu/${id_jeu}/commentaire`;
     const commentaireData = {
       commentaire: this.commentaireForm.value.commentaire,
+      date_com: new Date(),
       note: this.commentaireForm.value.note,
-      etat: 'public',
-      date_com: new Date()
-    }
+      jeu_id: this.jeu?.id,
+      user_id: this.user_id,
+      etat: 'public'
+    };
     this.http.post(url, commentaireData)
       .subscribe(
         (response) => {
@@ -75,13 +97,7 @@ export class CommentModalComponent {
   }
 
   cancel(): void {
-    // Annuler la saisie du commentaire et fermer la fenêtre modale
     this.dialogRef.close();
   }
 
-  onSubmit() {
-    if (this.commentaireForm.valid) {
-      this.saveComment();
-    }
-  }
 }
